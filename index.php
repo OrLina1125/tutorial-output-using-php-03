d55776981dc52b57aafb4bb01e9b37182b6f4b5c
Orlina M Brabar <90068655+OrLina1125@users.noreply.github.com>
2022-03-26 11:48:00 +0900

    add

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title of the document</title>
+</head>
+<body>
+<form">
+<input type ="text" name="person">
+</form>
+
+    <?php
+        $name = "Daniel";
+        echo $name."Daniel is a handsame fellow!";
+    ?>
+
+</body>
+</html>
